01a0d77fd5bf7424c84af96b7abdc840996ccb5b
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-11-26 12:07:07 -0700

    add arrow to arrow func

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import CartItem from '../CartItem/CartItem'
 
-const Cart = ({ books, total }) (
+const Cart = ({ books, total }) => (
     <div>{books.filter(book => book.inCart)
         .map((book, idx) => {
             return <CartItem key={idx} book={book}/>
