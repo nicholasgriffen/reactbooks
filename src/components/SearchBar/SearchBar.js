4c82cd4fe85a440bc183ff11a4a565fee5fa8dfc
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-11-26 17:08:29 -0700

    refactor state to props in search bat

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,40 +1,19 @@
 import React, { Component } from 'react'
 
 
-class SearchBar extends Component {
-    constructor() {
-        super()
-
-        this.state = {
-            input: '',
-        }
-    }
-    
-    findBook = (e) => {
-        e.preventDefault()
-
-        const searchTerm = e.target.value
-        const { onSearchBook } = this.props
-
-        this.setState({
-            ...this.state,
-            input: searchTerm,
-        })
-
-        onSearchBook(searchTerm)
-    }
-
-    render() {
-        return (
-            <div>
-                <form>
-                    <label>Search:
-                        <input type="search" value={this.state.input} onChange={this.findBook}/>
-                    </label>
-                </form>
-            </div>
-        )
-    }
-}
+const SearchBar = ({ onSearchBook, input })=> (
+    <div>
+        <form>
+            <label>Search:
+                <input type="search" 
+                    value={ input } 
+                    onChange={ e => {
+                        e.preventDefault()
+                        onSearchBook(e.target.value)
+                    }}/>
+            </label>
+        </form>
+    </div>
+)
 
 export default SearchBar
